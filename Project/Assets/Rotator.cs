501edb1965c6d7c41a819a20be803dcf8bb62483
manji369 <manji369@gmail.com>
2016-10-10 16:12:58 -0700

    Release 1.0

---
+++ Project/Assets/Rotator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		transform.Rotate (new Vector3 (0, 0, -moveHorizontal*100) * Time.deltaTime);
+	}
+}
---
+++ Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	private bool gameOver;
+	private int score;
+
+	public GUIText scoreText;
+	public GUIText restartText;
+	public GUIText gameOverText;
+
+	void Start()
+	{
+		gameOver = false;
+		restartText.text = "";
+		gameOverText.text = "";
+		score = 0;
+		UpdateScore ();
+	}
+
+	void FixedUpdate ()
+	{
+		//float moveHorizontal = Input.GetAxis("Horizontal");
+		//float moveVertical = Input.GetAxis("Vertical");
+		Vector3 movement = new Vector3 (0.0f, 0.0f, 0.9f);
+		rigidbody.AddForce (movement * speed * Time.deltaTime);
+		rigidbody.position = new Vector3 
+			(
+				rigidbody.position.x,
+				Mathf.Clamp (rigidbody.position.y, -5f, -0.27f),
+				rigidbody.position.z
+				);
+	
+	}
+
+	void Update()
+	{
+		if(gameOver)
+		{
+			restartText.text = "Press 'R' to Restart";
+			Vector3 movement = new Vector3 (0.0f, 0.0f, -0.9f);
+			rigidbody.AddForce (movement * speed * Time.deltaTime);
+			if(Input.GetKeyDown (KeyCode.R))
+			{
+				Application.LoadLevel (Application.loadedLevel);
+			}
+		}
+	}
+	
+
+	public void AddScore (int newScoreValue)
+	{
+		score += newScoreValue;
+		UpdateScore ();
+	}
+	
+	void UpdateScore()
+	{
+		scoreText.text = "Score: " + score;
+	}
+
+	public void GameOver()
+	{
+		gameOverText.text = "Game Over!";
+		gameOver = true;
+	}
+}
